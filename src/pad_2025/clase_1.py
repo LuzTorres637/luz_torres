092e311eac8c1a3f3691759cbbb9fb6ae15af5f8
LuzTorres637 <luz.torres@est.iudigital.edu.co>
2025-02-16 16:06:19 -0500

    clase1 - programacion orientacion de objetos

---
+++ src/pad_2025/clase_1.py
@@ -0,0 +1,18 @@
+# class - relacion directa con el objeto (entidad o objeto)
+# def - acciones o funciones del obejto
+
+class Personas():
+    def __init__(self):
+    # atributos del obejto
+        self.nombre = "Andres"
+        self.edad = 0
+        self.estatura = 100.0 
+        self.peso = 35.6
+    
+        
+persona =Personas()  
+persona.edad = 42
+persona.estatura =193 
+print(persona.nombre) 
+print("nombre: ",persona.nombre," edad: ",persona.edad, " cms: ", persona.estatura)
+       
