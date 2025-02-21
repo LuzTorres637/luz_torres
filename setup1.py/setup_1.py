afb7080a3d45a30c58253333b4e394b354f17702
LuzTorres637 <luz.torres@est.iudigital.edu.co>
2025-02-21 16:38:03 -0500

    prueba_1_5

---
+++ setup1.py/setup_1.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Luz Andrea Torres Usme",
+    author_email="luz.torres@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
