104362b62a9b57d9abe276724d00072115ba1cec
LuzTorres637 <luz.torres@est.iudigital.edu.co>
2025-02-21 15:43:48 -0500

    clase_1_2

--- src/pad_2025/ingestiones.py
+++ src/pad_2025/ingestiones.py
@@ -29,10 +29,55 @@ inges = Ingestiones()
 datos_txt = inges.leer_json()        
 print(datos_txt)
 
-#imprimir los dos
+#leer varios
+
+def leer_varios_txt(self,nombre=""):
+    
+        ruta_txt = "{}txt/{}".format(self.ruta_static,nombre)
+        datos=""
+        with open(ruta_txt,"w",encoding="utf-8") as f :
+            datos = f.read()      
+        return datos         
+                      
+nombre_archivo ="info copy.txt"
+datos_txt = inges.leer_varios_txt(nombre_archivo)        
+print(datos_txt)
+
+#imprimir los tres
 
 datos_txt = inges.leer_json()        
 print(datos_txt)
 print("**********")
 datos_json= inges.leer_json()
 print(datos_json)
+print("**********")
+nombre_archivo ="info copy.txt"
+datos_txt = inges.leer_varios_txt(nombre_archivo)        
+print(datos_txt)
+
+#funciones para leer
+
+def leer_cualquier_excel(self,nombre)
+    pass
+def leer_cualquier_csv(self,nombre)
+    pass
+def leer_bd(self,nombre_bd="",servidor="",puerto=0000):
+    pass
+def leer_api(self,url=""):
+    pass
+def leer_html(self,url=""):
+    pass
+
+#imprimir
+
+def escribir_txt(self,nombre,datos):
+    
+        ruta_txt = "{}.txt".format(nombre)
+        datos=""
+        with open(ruta_txt,"w",encoding="utf-8") as f :
+            #f.write(datos)  
+            f.writable (datos)   
+              
+                      
+inges.escribir_txt(nombre="archivo_txt",datos=datos_txt) 
+
