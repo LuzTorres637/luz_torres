d4038517fdd1c2d8eb793d855e75f665a6701951
LuzTorres637 <luz.torres@est.iudigital.edu.co>
2025-03-14 19:04:08 +0000

    revision_5

--- src/pad_2025/actividad_final_2/actividad_final_2.py
+++ src/pad_2025/actividad_final_2/actividad_final_2.py
@@ -28,7 +28,7 @@ class Actividad_2:
 #Genera un array de NumPy con valores desde 10 hasta 29.
     def punto_1(self, inf=10, sup=29):   
         array_10_29 = np.arange(inf,sup) 
-        self.df.loc[0,"valor"] = str(array_10_29)
+        self.df.loc[0,"valor"] = f"{array_10_29}"
 
 #Calcula la suma de todos los elementos en un array de NumPy de tamaño 10x10, lleno de unos.    
     def punto_2(self):
